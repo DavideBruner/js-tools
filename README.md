3e036312e7516b4bdb1dc2615121b59e5df5d0c7
DavideBruner <davidebruner@hotmail.it>
2020-01-13 15:54:08 +0100

    chore: init packages

---
+++ README.md
@@ -0,0 +1,6 @@
+# @davidebruner js toolbox
+
+**Includes:**
+
+- [@davidebruner/eslint-config](https://github.com/davidebruner/js-tools/tree/master/packages/eslint-config)
+- [@davidebruner/prettier-config](https://github.com/davidebruner/js-tools/tree/master/packages/prettier-config)
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,14 @@
+// .prettierrc.js
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+}
